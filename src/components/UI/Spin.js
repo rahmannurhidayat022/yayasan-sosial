53bb02c80acb15f768167b60df7151df3bae5f5a
rahmannurhidayat022 <rahmannurhidayat022@gmail.com>
2022-10-30 10:21:55 +0700

    add memo to Spin Component

--- src/components/UI/Spin.js
+++ src/components/UI/Spin.js
@@ -1,7 +1,8 @@
+import { memo } from 'react';
 import { CgSpinner } from 'react-icons/cg';
 
 const Spin = ({ className, size = 24 }) => {
 	return <CgSpinner className={`animate-spin ${className}`} size={size} />;
 };
 
-export default Spin;
+export default memo(Spin);
